016fb2faee598a878f751e270f8c2632fd33058c
y-Yamao-cc-20260302 <yuta.yamao.carecon@gmail.com>
2026-06-26 18:39:42 +0900

    DeletePictureTest,UplodePictureTestの作成

---
+++ backend/tests/Feature/DeletePictureTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Storage;
+use Illuminate\Http\UploadedFile;
+use App\Helpers\ImageHelper;
+
+class DeletePictureTest extends TestCase
+{
+    // テスト実行するたびにデータベースをロールバックまでしてくれる機能
+    use RefreshDatabase;
+
+    // 引数は、データ型を特定するため、array型と定義
+
+    // 写真の削除ができる
+    public function test_delete_picture()
+    {
+        // エラーを出力してくれる
+        $this->withoutExceptionHandling();
+        // 画像アップロード用のストレージを作成
+        Storage::fake('public');
+        // ダミー画像を作成
+        $picture = UploadedFile::fake()->image('image.png', 200, 200)->size(3072);
+        // ファイル名を生成し、public/pictureフォルダに保存する
+        $filename = $picture->hashName();
+        Storage::disk('public')->putFileAs('picture', $picture, $filename);
+        // ダミー画像のファイルパスを作成
+        $picturePath = 'picture/' . $filename;
+        // データがフォルダに保存されているかを確認 
+        // ※ただ初めから保存されていなくてassertMissingがtrueにならないように対策
+        Storage::disk('public')->assertExists($picturePath);
+        // オブジェクトとして生成
+        $imageHelper = new ImageHelper();
+        // uploadePictureを呼び出してファイルパスを取得
+        $imageHelper->deletePicture($picturePath);
+        // データがフォルダから消えているかを確認する
+        Storage::disk('public')->assertMissing($picturePath);
+    }
+
+
+    // 
+    // データプロバイダのアノテーション()が必要。　しゃーぷ[DataProvider('関数名')]と書き、インポートもする。
+    #[DataProvider('ProvideDateProvider')]
+    // 型宣言せずに動的に型を設定する(文字列でないものを渡すパターンもあるため)
+    public function test_no_delete_picture($delete_img_path,)
+    {
+        // エラーを出力してくれる
+        $this->withoutExceptionHandling();
+        // 画像アップロード用のストレージを作成
+        Storage::fake('public');
+
+        // ダミー画像を作成
+        $picture = UploadedFile::fake()->image('image.png', 200, 200)->size(3072);
+        $filename = $picture->hashName();
+        Storage::disk('public')->putFileAs('picture', $picture, $filename);
+
+        // uploadePictureを呼び出してファイルパスを取得
+        $img_path = 'picture/' . $filename;
+
+        // オブジェクトとして生成
+        $imageHelper = new ImageHelper();
+        // 引数$delete_img_pathを引数として、削除を呼び出す
+        $imageHelper->deletePicture($delete_img_path);
+        // すでに保存済みのデータが消えていないことで、削除がされていないことを確認する
+        Storage::disk('public')->assertExists($img_path);
+    }
+
+    public static function ProvideDateProvider(): array
+    {
+        return [
+            'emptyPath' => ['delete_img_path' => '',],
+            'noString' => ['delete_img_path' => 1,],
+            'noexistpath' => ['delete_img_path' => 'picture/noimagenapass',],
+        ];
+    }
+}
---
+++ backend/tests/Feature/UplodePictureTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Storage;
+use Illuminate\Http\UploadedFile;
+use App\Helpers\ImageHelper;
+
+class UplodePictureTest extends TestCase
+{
+    // テスト実行するたびにデータベースをロールバックまでしてくれる機能
+    use RefreshDatabase;
+
+    // テストケース24に相当　正常に画像登録ができる
+    public function test_uplode_picture()
+    {
+        // エラーを出力してくれる
+        $this->withoutExceptionHandling();
+        // 画像アップロード用のストレージを作成
+        Storage::fake('public');
+        // ダミー画像を作成
+        $picture = UploadedFile::fake()->image('image.png', 200, 200)->size(3072);
+        // オブジェクトとして生成
+        $imageHelper = new ImageHelper();
+        // uploadePictureを呼び出してファイルパスを取得
+        $img_path = $imageHelper->uplodePicture($picture);
+        // ファイルパスを参照できる形に修正
+        $img_path = 'picture/' . $img_path;
+        // データがフォルダに保存されているかを確認
+        Storage::disk('public')->assertExists($img_path);
+    }
+
+    #[DataProvider('ProvideDateProvider')]
+    public function test_dont_uplode_picture(object $img)
+    {
+        // エラーを出力してくれる
+        $this->withoutExceptionHandling();
+        // 画像でないオブジェクトでは、Call to undefined methodのエラーになるため。
+        $this->expectExceptionMessage('Call to undefined method stdClass::store()');
+        // オブジェクトを作成
+        $imagehelper = new ImageHelper();
+        // 画像アップロード関数を呼び出す
+        $imagehelper->uplodePicture($img);
+    }
+
+    public static function ProvideDateProvider(): array
+    {
+        return [
+            // テストケース25に相当　画像ではないものを入力値とする
+            'noImageObject' => ['img' => (object)['name' => 'a',]],
+            // テストケース26に相当　空のオブジェクトを引数として呼び出す
+            // が、空のオブジェクトで渡してもhelper側がfalseにならない。
+            'emptyObject' => ['img' => (object)[],],
+        ];
+    }
+}
