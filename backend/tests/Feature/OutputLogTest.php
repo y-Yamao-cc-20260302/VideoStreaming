cef40be93633a6a936d7064536362a83bc15edaa
y-Yamao-cc-20260302 <yuta.yamao.carecon@gmail.com>
2026-07-01 13:15:55 +0900

    ErrorLogHelper,OutputLogTestの修正

--- backend/tests/Feature/OutputLogTest.php
+++ backend/tests/Feature/OutputLogTest.php
@@ -3,12 +3,13 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Helpers\ErrorHelper;
+use App\Helpers\ErrorLogHelper;
 use Illuminate\Support\Facades\Log;
 use Exception;
 
 class OutputLogTest extends TestCase
 {
+    // テストケース31
     public function test_output_log()
     {
         // Log::spy()にてログ確認する
@@ -17,24 +18,14 @@ class OutputLogTest extends TestCase
         $message = 'testException';
         // Exceptionを作成（この行のファイル名と行番号が自動でセットされる）
         $testException = new Exception($message);
-
-        // testExceptionからfileとlineの値を取得する
-        $file = $testException->getFile();
-        $line = $testException->getLine();
-
-        // ErrorHelperを呼び出す
-        $helper = new ErrorHelper();
+        // ErrorLogHelperを呼び出す
+        $helper = new ErrorLogHelper();
         $helper->outputLog($testException);
-
         // spyで確認したい内容をshouldHaveReceivedで確認する
         Log::shouldHaveReceived('error')
             // 一度だけ呼び出されたことを確認する
             ->once()
             // 内容を確認、'エラーが発生しました。'に加え、outputLogと同じ内容が書き込まれているかを確認する
-            ->with('エラーが発生しました。', [
-                'message' => $message,
-                'file'    => $file,
-                'line'    => $line,
-            ]);
+            ->with($testException->getMessage(), ['exception' => $testException,]);
     }
 }
